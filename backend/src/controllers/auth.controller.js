c2edc0aec8ced2569f6660a5396948d8a39d0073
Patrick <patrickbpds@gmail.com>
2025-03-20 18:28:20 -0300

    signup update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -21,13 +21,23 @@ export const signup = async (req, res) => {
         })
 
         if (newUser) {
+            generatedToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            })
 
         } else {
             res.status(400).json({ message: "Invalid user data" });
         }
 
     } catch (error) {
-
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal server Error" });
     }
 
 };
