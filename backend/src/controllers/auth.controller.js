0a2f412a89cce585476ac6deea50c8d71b2499bd
Patrick <patrickbpds@gmail.com>
2025-03-21 10:40:33 -0300

    signup to stay updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,27 +1,33 @@
+import { generatedToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
-    const { fullName, email, password } = req.body
+    const { fullName, email, password } = req.body;
     try {
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters" });
         }
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email });
 
         if (user) return res.status(400).json({ message: "Email already exists" });
 
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
             fullName,
             email,
             password: hashedPassword
-        })
+        });
 
         if (newUser) {
-            generatedToken(newUser._id, res)
+            generatedToken(newUser._id, res);
             await newUser.save();
 
             res.status(201).json({
@@ -29,7 +35,7 @@ export const signup = async (req, res) => {
                 fullName: newUser.fullName,
                 email: newUser.email,
                 profilePic: newUser.profilePic,
-            })
+            });
 
         } else {
             res.status(400).json({ message: "Invalid user data" });
