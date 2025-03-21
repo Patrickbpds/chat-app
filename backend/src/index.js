b9a0b3a3efa72ea54de115ab893fb82523db54d0
Patrick <patrickbpds@gmail.com>
2025-03-21 12:54:36 -0300

    complete login & logout & update profile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT)
-    connectDB()
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
 });
